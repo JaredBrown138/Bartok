7c5e59195f5f294029b57d43dbef08a2b2d15636
Jared Brown <jbrown@chq.org>
2019-09-22 18:49:18 -0400

    adding django-jet

--- bartok/bartok/settings.py
+++ bartok/bartok/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jet.dashboard',
+    'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+JET_INDEX_DASHBOARD = 'bartok.dashboard.CustomIndexDashboard'
+JET_APP_INDEX_DASHBOARD = 'dashboard.CustomAppIndexDashboard'
