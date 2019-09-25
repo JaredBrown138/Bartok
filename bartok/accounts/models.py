c897f07baa321a0bc603d73ca5975a436a209d7c
Jared Brown <jbrown@chq.org>
2019-09-24 20:23:53 -0400

    adding user properties to Account model

--- bartok/accounts/models.py
+++ bartok/accounts/models.py
@@ -5,3 +5,19 @@ from django.contrib.auth.models import User
 class Account(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    @property
+    def first_name(self):
+        return self.user.first_name
+
+    @property
+    def last_name(self):
+        return self.user.last_name
+
+    @property
+    def email(self):
+        return self.user.email
+
+    @property
+    def username(self):
+        return self.user.username
